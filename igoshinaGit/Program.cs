764db2d767891b34b9b617d5fde7feeee1f79a09
almazikk999 <104065830+almazikk999@users.noreply.github.com>
2022-04-20 14:20:00 +0300

    Add files via upload

---
+++ igoshinaGit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace igoshinaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Маша крутая!";
+            Console.WriteLine(a);
+            int b = 5;
+            Console.WriteLine($"Поставьте {b}");
+        }
+    }
+}
